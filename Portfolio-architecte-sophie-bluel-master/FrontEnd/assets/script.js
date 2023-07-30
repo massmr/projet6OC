0f575ac61ce325f3a8258899c50a49d0c2f4f9ac
Massimo Marcellin <marcellin.massimo@gmail.com>
2023-07-31 00:38:13 +0100

    end logging logic and begin header logged

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,6 +1,6 @@
 //Front end script for Sophie Bluel Portfolio website
 
-//GET projects and save data in localStorage
+//GET API and save data in localStorage_______________________________________________________________
 let projects = window.localStorage.getItem('projects');
 if (projects === null) {
     const response = await fetch("http://localhost:5678/api/works");
@@ -12,13 +12,13 @@ if (projects === null) {
 else {
     projects = JSON.parse(projects);
 }
-//counter to check if page was already charged
+//counter to check if page was already charged IOT to ativate first button in green
 let alreadyLogged = false;
 
-//first call
+//first call to create first project grid
 createArticle(projects);
 
-//Create Projects elements
+//Create Projects elements_________________________________________________________________________________
 function createArticle(projects) {
 
     //For each project
@@ -35,20 +35,44 @@ function createArticle(projects) {
 
         //Append Child
         const gallery = document.querySelector(".gallery");
-        gallery.appendChild(articleFigure);
+        //check to prevent errors when script is invoked in other html page
+        if (gallery) {
+            gallery.appendChild(articleFigure);
+        }
         articleFigure.appendChild(figureImage);
         articleFigure.appendChild(figureCaption);
     }
 
     if (alreadyLogged === false) {
         const buttonActivated = document.getElementById("button1")
-        buttonActivated.classList.add("button_Selected");
+        //check to prevent errors when script is invoked in other html page
+        if (buttonActivated) {
+            buttonActivated.classList.add("button_Selected");
+        }
     }
     alreadyLogged = true;
 
 }
 
-//filters :
+//Create Login elements___________________________________________________________________________________
+const loggedData = localStorage.getItem("logged");
+let logged = loggedData ? JSON.parse(loggedData) : {test: false};
+
+if (logged.test === true) {
+    console.log("logged = " + logged.test);
+    createLoginElements();
+}
+function createLoginElements() {
+
+    const loggedinContainer = document.createElement("section");
+    loggedinContainer.classList.add("logged_container");
+    const header = document.querySelector(".header");
+    header.classList.add("header_logged");
+
+    header.prepend(loggedinContainer);
+}
+
+//filters__________________________________________________________________________________________________
 
 //declare arrays => one iteration per button
 const buttonId = [];
@@ -57,30 +81,33 @@ const projectCategory = [];
 //create a filter function for each button
 for (let i = 1, numButton = 4; i < numButton + 1; ++i) {
     buttonId[i] = document.getElementById("button" + i);
-    //event listener on each button
-    buttonId[i].addEventListener("click", function () {
 
-        //Unselect every button /Select new button
-        for (let j = 1, numButton = 4; j < numButton + 1; ++j) {
-            buttonId[j].classList.remove("button_Selected");
-        }
-        buttonId[i].classList.add("button_Selected");
+    //check to prevent errors when script is invoked in other html page
+    if (buttonId[i]) {
+        //event listener on each button
+        buttonId[i].addEventListener("click", function () {
 
-        //display all projects => invoke first function
-        if (i === 1) {
-            document.querySelector(".gallery").innerHTML = "";
-            createArticle(projects);
-        }
+            //Unselect every button /Select new button
+            for (let j = 1, numButton = 4; j < numButton + 1; ++j) {
+                buttonId[j].classList.remove("button_Selected");
+            }
+            buttonId[i].classList.add("button_Selected");
 
-        //filter functions
-        else {
-            projectCategory[i] = projects.filter(function (e) {
-                return e.categoryId === i - 1;
-            })
+            //display all projects => invoke first function
+            if (i === 1) {
+                document.querySelector(".gallery").innerHTML = "";
+                createArticle(projects);
+            }
 
-            document.querySelector(".gallery").innerHTML = "";
-            createArticle(projectCategory[i]);
-        }
-    })
+            //filter functions
+            else {
+                projectCategory[i] = projects.filter(function (e) {
+                    return e.categoryId === i - 1;
+                })
 
+                document.querySelector(".gallery").innerHTML = "";
+                createArticle(projectCategory[i]);
+            }
+        })
+    }
 }
