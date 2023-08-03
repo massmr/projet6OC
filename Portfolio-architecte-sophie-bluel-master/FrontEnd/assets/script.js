4a8f11c9e657b653d169cf3dbf65d8b42552d544
Massimo Marcellin <marcellin.massimo@gmail.com>
2023-08-03 18:59:00 +0100

    add feature : delete projects and preview it

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -21,19 +21,19 @@ let alreadyLogged = false;
 createArticle(projects);
 
 //Create Projects elements_________________________________________________________________________________
-function createArticle(projects) {
+function createArticle(el) {
 
-    //For each project
-    for (let i = 0; i < projects.length; ++i) {
+    //For each el
+    for (let i = 0; i < el.length; ++i) {
 
         //Create figure
         const articleFigure = document.createElement("figure");
         //Create image
         const figureImage = document.createElement("img");
-        figureImage.src = projects[i].imageUrl;
+        figureImage.src = el[i].imageUrl;
         //Create caption
         const figureCaption = document.createElement("figcaption");
-        figureCaption.innerText = projects[i].title;
+        figureCaption.innerText = el[i].title;
         const portfolioTitle = document.getElementById("portfolio_title");
         portfolioTitle.classList.add("portfolio_title-notLogged");
 
@@ -181,27 +181,32 @@ for (let i = 1, numButton = 4; i < numButton + 1; ++i) {
 
 //Modale_______________________________________________________________________________________
 
-//create overlay
+//All modale is made out of this array because it is only temporary and waits to be POSTed
+let preview = projects.map(el => el);
+
+//______________________|||||||create overlay||||||||__________________________________________
+
 const overlay = document.createElement("div");
 overlay.classList.add("overlay");
 
 const body = document.body;
 body.prepend(overlay);
 
-//display overlay + modale
+//display overlay + 1st time modale
 const modifyButton = document.querySelector(".modify_button");
 if(modifyButton) {
     modifyButton.addEventListener("click", function() {
-        createModaleElements(projects)
+        //always invoke with preview to prevent reinvoking wrong array after project deletion
+        //preview = projects duplication
+        createModaleElements(preview);
     } );
 }
 
-//create elements
-function createModaleElements(projects){
-    //control state of modal
-    //window.localStorage.setItem("modaleState", JSON.stringify(true));
+//_____________||||||||||||||create elements|||||||||||||____________________________________
+function createModaleElements(el){
     //activate overlay
-    document.querySelector(".overlay").style.display = 'block';
+    const overlay = document.querySelector(".overlay");
+    overlay.style.display = 'block';
 
     //create modale
     const modaleContainer = document.createElement("article");
@@ -233,45 +238,49 @@ function createModaleElements(projects){
     modaleButtonContainer.appendChild(modaleButtonAdd);
     modaleButtonContainer.appendChild(modaleButtonDelete);
 
-    //display projects
-    for (let i = 0; i < projects.length; ++i) {
-        //Create figures
-        //Tips : modaleFigureContainer is here only to contain trash icon when position absolute :)
-        const modaleFigureContainer = document.createElement("div");
-        modaleFigureContainer.classList.add("modale_figure-container")
-        const modaleFigure = document.createElement("figure");
-        modaleFigure.classList.add("modale_figure");
-        //Create image
-        const figureImage = document.createElement("img");
-        figureImage.src = projects[i].imageUrl;
-        figureImage.classList.add("modale_image");
-        //Create caption
-        const figureCaption = document.createElement("p");
-        figureCaption.innerText = "éditer";
-        //create delete icon
-        const deleteIcon = document.createElement("i");
-        deleteIcon.classList.add("fa-solid");
-        deleteIcon.classList.add("fa-trash-can");
-        deleteIcon.classList.add("trash")
+    //create modale projects
+    function createModaleGallery(el) {
+        for (let i = 0; i < el.length; ++i) {
+            //Create figures
+            //Tips : modaleFigureContainer is here only to contain trash icon when position absolute :)
+            const modaleFigureContainer = document.createElement("div");
+            modaleFigureContainer.classList.add("modale_figure-container")
+            modaleFigureContainer.id = "figure_" + (i + 1); //important to erase project in modale
+            const modaleFigure = document.createElement("figure");
+            modaleFigure.classList.add("modale_figure");
+            //Create image
+            const figureImage = document.createElement("img");
+            figureImage.src = projects[i].imageUrl;
+            figureImage.classList.add("modale_image");
+            //Create caption
+            const figureCaption = document.createElement("p");
+            figureCaption.innerText = "éditer";
+            //create delete icon
+            const deleteIcon = document.createElement("i");
+            deleteIcon.classList.add("fa-solid");
+            deleteIcon.classList.add("fa-trash-can");
+            deleteIcon.classList.add("trash")
+            deleteIcon.id = ("trash_" + (i + 1));
 
-        modaleGallery.appendChild(modaleFigureContainer);
-        modaleFigureContainer.appendChild(modaleFigure);
-        modaleFigure.appendChild(figureImage);
-        modaleFigure.appendChild(figureCaption);
-        modaleFigure.prepend(deleteIcon);
+            modaleGallery.appendChild(modaleFigureContainer);
+            modaleFigureContainer.appendChild(modaleFigure);
+            modaleFigure.appendChild(figureImage);
+            modaleFigure.appendChild(figureCaption);
+            modaleFigure.prepend(deleteIcon);
+
+        }
 
     }
 
+    createModaleGallery(preview);
     closeModale(modaleXmark);
+    closeModale(overlay);
+    deleteProjectModale(el);
 };
 
-//close modale
-//const xmark = document.querySelector(".xmark");
-
-//console.log(xmark);
+//___________||||||||||||||close modale|||||||||||||||||____________________________
 function closeModale(el) {
     if(el) {
-        console.log(2);
         el.addEventListener("click", function() {
             //hide overlay
             document.querySelector(".overlay").style.display = 'none';
@@ -279,8 +288,47 @@ function closeModale(el) {
             //erase modale
             const modaleContainer = document.querySelector(".modale");
             modaleContainer.remove();
+
+            //recreate new gallery updated
+            const gallery = document.querySelector(".gallery");
+            gallery.innerHTML = "";
+            createArticle(preview);
+
         });
     } else {
         return;
     }
 }
+
+
+//________||||||||||||||||Erase projects||||||||||||||||___________________________
+//make new array to preview :
+const trash = [];
+console.log(preview);
+
+//delete function : event listener on each trash icon
+//>> splice new Arr
+//>>callback create modal
+function deleteProjectModale(el) {
+    for (let i = 0; i < el.length; ++i) {
+        //ex : trash[0] == #trash_1; trash[1] == #trash_2 ...; trash[i] == #trash_(i + 1)
+        trash[i] = document.getElementById("trash_" + (i + 1) );
+        trash[i].addEventListener("click", function() {
+
+            //update preview[]
+            preview.splice((i), 1);
+            //update id's for all figures above of deleted one (i)
+            for (let j = 0; j < el.length;  ++j) {
+                preview[j].id = (j + 1);
+            }
+
+            //update gallery :
+            //rm old gallery
+            const projectRemoved = document.getElementById("figure_" + (i + 1));
+            projectRemoved.remove();
+            //recreate gallery with new preview
+            console.log(preview);
+        })
+    }
+}
+
