bc5eb3db22005ded1729e276c3841a966c839c9a
Massimo Marcellin <marcellin.massimo@gmail.com>
2023-08-18 22:51:15 +0100

    fix issue : apply same type to client userId and server userId

--- Portfolio-architecte-sophie-bluel-master/Backend/middlewares/auth.js
+++ Portfolio-architecte-sophie-bluel-master/Backend/middlewares/auth.js
@@ -2,17 +2,20 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
 	try {
-		console.log(req.headers.authorization);
 		const token = req.headers.authorization.split(' ')[1]
-		const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
+		const decodedToken = jwt.verify(token, 'thisIsMySecretKey')
 		const userId = decodedToken.userId
 		req.auth = { userId }
-		if (req.body.userId && req.body.userId !== userId) {
+		console.log('req.body.userId : ' + req.body.userId);
+		console.log(userId);
+		if (req.body.userId && String(req.body.userId) !== String(userId) ) {
+			console.log('invalid fucking userId');
 			throw 'Invalid user ID'
 		} else {
 			next()
 		}
 	} catch {
+		console.log('fuck it fuck it fuck it')
 		res.status(401).json({
 			error: new Error('You are not authenticated')
 		})
